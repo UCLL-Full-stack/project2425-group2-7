cc6086717a867120768a154604c6130a4e757317
Austyne <103767831+Austyne@users.noreply.github.com>
2024-11-03 17:16:30 +0100

    moved the add car button to the cars page

--- back-end/repository/cars.db.ts
+++ back-end/repository/cars.db.ts
@@ -8,6 +8,14 @@ const cars: Car[] =[
         model: "Corolla",
         condition: "New",
         status: "IN_STOCK"
+    }), 
+    new Car({
+        chasisNumber: 12,
+        price: 30000,
+        brand: "Toyota",
+        model: "Camry",
+        condition: "New",
+        status: "IN_STOCK"
     })
 
 ];
