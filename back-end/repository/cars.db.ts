f12a8cc0b11193e2787ae849694be7e00ce706e7
Austyne <103767831+Austyne@users.noreply.github.com>
2024-11-03 03:10:19 +0100

    added a car by default

--- back-end/repository/cars.db.ts
+++ back-end/repository/cars.db.ts
@@ -1,10 +1,20 @@
 import { Car } from "../model/car";
 
+const car1: Car = new Car({
+    chasisNumber: 12345,
+    price: 20000,
+    brand: "Toyota",
+    model: "Corolla",
+    condition: "New",
+    status: "IN_STOCK"
+});
 const cars: Car[] = [];
 
 const addCar = (car: Car) =>{
     cars.push(car);
 }
+
+addCar(car1);
 
 const getAllCars = () => {
     return cars;
