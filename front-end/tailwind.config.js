a10f1f656e1bd6265db374eace938b9f4247de39
Austyne <103767831+Austyne@users.noreply.github.com>
2024-12-11 15:17:32 +0100

    cretaed 2 new pages

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+    //   colors: {
+    //     // Add custom colors here if needed
+    //   },
+    //   screens: {
+    //     'sm': '640px',
+    //     'md': '768px',
+    //     'lg': '1024px',
+    //     'xl': '1280px',
+    //     '2xl': '1536px',
+    //   },
+    },
+  },
+  plugins: [],
+//   darkMode: 'class', // Enable dark mode variant
+}
