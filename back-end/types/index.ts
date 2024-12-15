9ea140176332741cf1eb34d8e7b9c066aa00d29f
kotami <145974605+xkotami@users.noreply.github.com>
2024-12-15 14:18:20 +0100

    added extra types for future usage to index.ts in backend

--- back-end/types/index.ts
+++ back-end/types/index.ts
@@ -10,6 +10,16 @@ type CarInput = {
     status: Status;
 }
 
+type User = { // to be used for fetching
+    id: number;
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    role: Role;
+}
+
 type UserInputRegister = {
     username: string;
     firstName: string;
@@ -24,6 +34,33 @@ type UserInputLogin = {
     password: string;
 }
 
+type LoyaltyCard = {
+    id: number;
+    points: number;
+    tier: Tier;
+    discountPercentage: number;
+    validity: Date;
+    totalSpent: number;
+}
+
+type Car = { // when needing raw Car object
+    id: number;
+    chassisNumber: number;
+    price: number;
+    brand: string;
+    model: string;
+    condition: string;
+    status: Status;
+}
+
+type Customer = { // when needing raw customer object
+    id: number;
+    loyaltyCard: LoyaltyCard;
+    user: User;
+    purchaseHistory: Transaction[];
+    cars: Car[]
+}
+
 type AuthenticationResponse = {
     token: string;
     username: string;
@@ -31,9 +68,23 @@ type AuthenticationResponse = {
     role: Role;
 }
 
-type JWTPayload = {
+type JWTPayload = { // role decides whats shown
     username: string;
     role?: Role
+}
+
+type TransactionInput = {
+    type: Type;
+    date: Date;
+    carsId: number;
+    customerId: number;
+}
+
+type Transaction = { // when needing raw transaction object
+    id: number;
+    type: Type;
+    cars: Car[];
+    customer: Customer
 }
 
 type Role = 'ADMIN' | 'CUSTOMER';
@@ -49,6 +100,12 @@ export {
     Role,
     AuthenticationResponse,
     JWTPayload,
+    Transaction,
+    TransactionInput,
+    Customer,
+    Car,
+    LoyaltyCard,
+    User
 }
 
 
