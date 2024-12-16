56abba8c6afd9b34e208599e0778438aba7ae1ec
kotami <145974605+xkotami@users.noreply.github.com>
2024-12-16 03:47:40 +0100

    minor error changes

--- back-end/repository/transactions.db.ts
+++ back-end/repository/transactions.db.ts
@@ -1,5 +1,6 @@
 import database from './database';
 import {Transaction} from "../model/transaction";
+import { TransactionInput } from '../types';
 
 const getAllTransactions = async () => {
     try {
@@ -10,8 +11,39 @@ const getAllTransactions = async () => {
                     }}}})
         return transactions.map((transaction) => Transaction.from(transaction));
     } catch(error) {
-        throw new Error("Problem with fetching in repository transactions")
+        throw error
     }
 };
 
-export default {getAllTransactions};
+const addTransaction = async (transactionInput: TransactionInput): Promise<Transaction> =>  {
+    try {
+        const transactionPrisma = await database.transaction.create({
+            data: {
+                typeTransaction: transactionInput.typeTransaction,
+                cars: {
+                    connect: transactionInput.carsId.map(id => ({id}))
+                },
+                customer: {
+                    connect: {id: transactionInput.customerId}
+                },
+                date: transactionInput.date,
+
+                // include related entities including the nested ones inside customer
+
+            }, include: {
+                cars: true,
+                customer: {include: {
+                    loyaltyCard: true,
+                        cars: true,
+                        user: true,
+                    }},
+            }
+        })
+        return Transaction.from(transactionPrisma);
+    } catch(error) {
+        console.log("Error in repository: " + error);
+        throw error;
+    }
+}
+
+export default {getAllTransactions, addTransaction};
