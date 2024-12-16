4134b1439ee967a514fe77f12cc3409e97da5c00
kotami <145974605+xkotami@users.noreply.github.com>
2024-12-16 03:46:17 +0100

    minor changes to swagger and model transaction

--- back-end/model/transaction.ts
+++ back-end/model/transaction.ts
@@ -6,37 +6,37 @@ import {
     User as UserPrisma,
 } from '@prisma/client';
 import {Car} from "./car";
-import {Type} from "../types";
+import {TypeTransaction} from "../types";
 import {Customer} from "./customer";
 export class Transaction{
     readonly id?: number;
-    readonly type: Type;
+    readonly typeTransaction: TypeTransaction;
     readonly date: Date;
     readonly cars: Car[];
     readonly customer: Customer;
 
 
-    static from ({id, type, date, cars=[], customer}: TransactionPrisma & {cars: CarPrisma[], customer: CustomerPrisma & {loyaltyCard: LoyaltyCardPrisma | null, cars: CarPrisma[], user: UserPrisma}}) {
+    static from ({id, typeTransaction: typeTransaction, date, cars=[], customer}: TransactionPrisma & {cars: CarPrisma[], customer: CustomerPrisma & {loyaltyCard: LoyaltyCardPrisma | null, cars: CarPrisma[], user: UserPrisma}}) {
         return new Transaction({id,
-            type: type as Type,
+            type: typeTransaction as TypeTransaction,
             date,
             cars: cars.map((car) => Car.from(car)),
             customer: Customer.from(customer)}
         );
     }
-    constructor(transaction:{id?: number, type: Type,date:Date, cars: Car[], customer: Customer}){
-        this.validate();
+    constructor(transaction:{id?: number, type: TypeTransaction,date:Date, cars: Car[], customer: Customer}){
         this.id = transaction.id;
-        this.type = transaction.type;
+        this.typeTransaction = transaction.type;
         this.date = transaction.date;
         this.cars = transaction.cars;
         this.customer = transaction.customer;
+        this.validate();
     }
     getId(): number | undefined{
         return this.id
     }
     getType(): 'P'| 'S' | 'T'{
-        return this.type
+        return this.typeTransaction
     }
     getDate(): Date{
         return this.date
@@ -52,26 +52,13 @@ export class Transaction{
 
     validate() {
 
-        switch(this.type) {
-            case 'P':
-                if (this.cars.length > 1) {
-                    throw new Error("You can only buy 1 car");
-                }
-                return null
-            case "T":
-                if (this.cars.length != 2) {
-                    throw new Error("You must submit your trade in vehicle and the vehicle you want to purchase");
-                }
-                return null
-            case "S":
-                if (this.cars.length != 1) {
-                    throw new Error("There can only be 1 car to sell");
-                }
-                return null
-            default:
-                throw new Error("Transaction must be either T, S or P")
-
+        if (this.typeTransaction == "T" && this.cars.length != 2) {
+            throw new Error("Specify 2 cars")
         }
+        if ((this.typeTransaction == "S" || this.typeTransaction == "P") && this.cars.length != 1) {
+            throw new Error("Specify 1 car")
+        }
+
 
     }
 
