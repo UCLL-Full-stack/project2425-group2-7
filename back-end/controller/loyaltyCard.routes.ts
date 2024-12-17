87fd276b6cf4c577bf76eb7a6c0ef811a2035219
kotami <145974605+xkotami@users.noreply.github.com>
2024-12-17 17:32:16 +0100

    added swagger for loyaltyCard get all

--- back-end/controller/loyaltyCard.routes.ts
+++ back-end/controller/loyaltyCard.routes.ts
@@ -1,8 +1,49 @@
 import express, { Request, Response, NextFunction } from 'express';
 import loyaltyCardService from "../service/loyaltyCard.service";
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     LoyaltyCard:
+ *         type: object
+ *         properties:
+ *           id:
+ *             type: number
+ *             format: int64
+ *           points:
+ *             type: number
+ *             format: int64
+ *           tier:
+ *             type: string
+ *             description: Bronze, Silver, Gold, Platinum
+ *           discountPercentage:
+ *             type: number
+ *             format: int64
+ *           validity:
+ *             type: string
+ *             format: date-time
+ *           totalSpent:
+ *             type: number
+ *             format: int64
+ */
 const loyaltyCardRouter = express.Router();
 
+/**
+ * @swagger
+ * /loyaltyCards:
+ *   get:
+ *     summary: Get a full list of all loyalty cards
+ *     responses:
+ *       200:
+ *         description: successfully return a list of all loyalty cards
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/LoyaltyCard'
+ */
 loyaltyCardRouter.get('/', async (req: Request, res: Response) => {
     try {
         const loyaltyCards = await loyaltyCardService.getAllLoyaltyCards();
